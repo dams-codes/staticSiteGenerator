c195756cce9fdc8da1679a5dcc3cbe19d8837994
cadams00 <cadams00@gmail.com>
2026-03-21 15:05:07 -0500

    saving for laptop move

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -19,6 +19,24 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                     newNodes.append(TextNode(splitOnDelimiter[i], text_type))    
     return newNodes
 
+def split_nodes_image(old_nodes):
+    images = []
+    for node in old_nodes:
+        print(extract_markdown_images(node))
+        # splitOnImage = node.text.split("!", 1)
+        # for i, part in enumerate(splitOnImage):
+        #     print(i, part)
+    #         if len(part) == 0:
+    #             continue
+    #         if i % 2 == 0:
+    #             images.append(TextNode(splitOnImage[i], TextType.TEXT))
+    #         else:
+    #             images.append(TextNode(extract_markdown_images(splitOnImage[i]), TextType.IMAGE))
+    # return images
+
+def split_nodes_link(old_nodes):
+    pass
+
 def extract_markdown_images(text):
     pattern = r"!\[([^\[\]]*)\]\(([^\(\)]*)\)"
     matches = re.findall(pattern, text)
@@ -39,10 +57,16 @@ def main():
     # print(new_nodes2)
     # for i in new_nodes:
     #     print(i)
-    text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
-    print(extract_markdown_images(text))
-    text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
-    print(extract_markdown_links(text))
+    # text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+    # print(extract_markdown_images(text))
+    # text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+    # print(extract_markdown_links(text))
+    node = TextNode(
+        "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+        TextType.TEXT,
+    )
+    new_nodes = split_nodes_image([node])
+    print(new_nodes)
 
 if __name__ == "__main__":
     main()
--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -3,6 +3,7 @@ from inline_markdown import (
     split_nodes_delimiter,
     extract_markdown_images,
     extract_markdown_links,
+    split_nodes_image,
 )
 
 from textnode import TextNode, TextType
@@ -104,6 +105,24 @@ class TestInlineMarkdown(unittest.TestCase):
                 ("another link", "https://wikipedia.org"),
             ],
             matches,
+        )
+
+    def test_split_images(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode(
+                    "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+                ),
+            ],
+            new_nodes,
         )
 
 if __name__ == "__main__":
